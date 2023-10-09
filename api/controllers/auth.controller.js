94dd7a12e0065908a09863388c6f26fa8d24f24c
RyanBestDev <ryansano22@gmail.com>
2023-10-08 19:20:47 -0700

    added eror handler middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
 	try {
 		const { username, password, email } = req.body;
 		const hashedPassword = bcrypt.hashSync(password, 10);
@@ -10,6 +10,6 @@ export const signup = async (req, res) => {
 
 		res.status(201).json({ message: 'User created successfully' });
 	} catch (err) {
-		res.status(500).json(err.message);
+		next(err);
 	}
 };
--- api/index.js
+++ api/index.js
@@ -10,6 +10,16 @@ app.use(express.json());
 app.use('/api/users', userRouter);
 app.use('/api/auth', authRouter);
 
+app.use((err, req, res, next) => {
+	const statusCode = err.statusCode || 500;
+	const message = err.message || 'Internal Server Error';
+	return res.status(statusCode).json({
+		success: false,
+		statusCode,
+		message,
+	});
+});
+
 mongoose
 	.connect(String(process.env.MONGO_STRING))
 	.then(() => console.log('connected to database'))
