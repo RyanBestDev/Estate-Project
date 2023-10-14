7ec1ac7ae29a045962e0562745ede7961c74771e
RyanBestDev <ryansano22@gmail.com>
2023-10-13 20:44:39 -0700

    updated header for profile + private profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import {
+	createBrowserRouter,
+	RouterProvider,
+	Outlet,
+	Navigate,
+	createRoutesFromElements,
+	Route,
+} from 'react-router-dom';
 import Home from './pages/Home';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
 	const router = createBrowserRouter([
@@ -40,11 +48,51 @@ export default function App() {
 				},
 				{
 					path: '/profile',
-					element: <Profile />,
+					element: <PrivateRoute />,
+					children: [{ path: '', element: <Profile /> }],
 					//loader: teamLoader,
 				},
 			],
 		},
 	]);
+
 	return <RouterProvider router={router} />;
 }
+//const router = createBrowserRouter(
+// 	createRoutesFromElements(
+// 		<>
+// 			<Route
+// 				path='/'
+// 				element={
+// 					<>
+// 						<Header />
+// 						<Outlet />
+// 					</>
+// 				}
+// 			>
+// 				<Route
+// 					path='/'
+// 					element={<Home />}
+// 				/>
+// 				<Route
+// 					path='/sign-in'
+// 					element={<SignIn />}
+// 				/>
+// 				<Route
+// 					path='/sign-up'
+// 					element={<SignUp />}
+// 				/>
+// 				<Route
+// 					path='/about'
+// 					element={<About />}
+// 				/>
+// 				<Route element={<PrivateRoute />}>
+// 					<Route
+// 						path='/profile'
+// 						element={<Profile />}
+// 					/>
+// 				</Route>
+// 			</Route>
+// 		</>
+// 	)
+// );
